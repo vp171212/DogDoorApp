841f6f7245f82d10b92db9750106be9166c45fd4
Ved Prakash <91253410+vp171212@users.noreply.github.com>
2023-10-05 22:27:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using DogDoor.Model;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogDoor
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int returnBeforeDoorClose = 2000;
+            int returnAfterDoorClose = 10000;
+            bool ownersResponsive = true;
+
+
+            DogDoorSimulator.DogDoorTestPath(returnBeforeDoorClose, ownersResponsive);
+        
+            DogDoorSimulator.DogDoorTestPath(returnAfterDoorClose, ownersResponsive);
+
+
+            DogDoorSimulator.DogDoorTestPath(returnBeforeDoorClose, !ownersResponsive);
+        
+            DogDoorSimulator.DogDoorTestPath(returnAfterDoorClose, !ownersResponsive);
+
+            Console.WriteLine("\nTest passed! Press any key to continue.\n");
+
+            Console.ReadKey();
+        }
+    }
+}
